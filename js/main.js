95c3a018d391c50f048c9a49df06eed7d3717ee8
George Beckwith <george.jbeckwith@googlemail.com>
2017-07-17 11:44:54 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ var words = ['Blue', 'Red', 'Yellow', 'Green', 'Orange', 'Purple', 'Pink', 'Whit
 //Randomise the variables for output.
 var targetWord = words[Math.floor(Math.random() * words.length)];
 
-//console.log("Target word: " + targetWord)
+console.log("Target word: " + targetWord)
 //Splits the word into an array of each character.
 var targetWordState = targetWord.toLowerCase().split("");
 
@@ -28,11 +28,7 @@ for (var i = 0; i < targetWord.length; i++) {
 document.getElementById("letters-container").innerHTML = gameWord;
 
 //Lives remainng.
-livesRemaining = (6 - guessCount)
-
-
-
-
+livesRemaining = (11 - guessCount)
 
 //Output the users input and link to function checkGuess.
 function userGuess() {
@@ -80,7 +76,7 @@ function checkGuess(guess) {
         document.getElementById("letters-container").innerHTML = gameWord;
 
         // CHECK TO SEE IF GAME HAS BEEN WON
-        document.getElementById("lives-remain").innerHTML = (6 - guessCount);
+        document.getElementById("lives-remain").innerHTML = (11 - guessCount);
         document.getElementById("lives-used").innerHTML = guessCount;
 
         if (gameWord.toLowerCase() == targetWord.toLowerCase()){
@@ -90,17 +86,21 @@ function checkGuess(guess) {
         }
     } else {
         //Make the guessCount increment every time that the user gets a letter wrong.
+        functionsArray[guessCount]()
         guessCount = guessCount + 1;
+
+
+
         //console.log("Lives used: " + guessCount)
 
             notInWord = notInWord + guess;
             document.getElementById("failedChar").innerHTML = "Used Characters: " + notInWord;
 
         //When the user gets 6 attempts wrong it will end the game.
-        document.getElementById("lives-remain").innerHTML = (6 - guessCount);
+        document.getElementById("lives-remain").innerHTML = (11 - guessCount);
         document.getElementById("lives-used").innerHTML = guessCount;
 
-        if (guessCount == 6) {
+        if (guessCount == 11) {
             document.getElementById("gameLost").innerHTML = "Game Lost!";
 
             textStop()
@@ -109,7 +109,7 @@ function checkGuess(guess) {
     }
 }
 
-//The index that the user has guessed is then replaced with a underscore call at setCharAt (line: 73)
+//The index that the user has guessed is then replaced with a underscore call at setCharAt (line: 74)
 function setCharAt(str,index,chr) {
     if(index > str.length-1) return str;
         return str.substr(0,index) + chr + str.substr(index+1);
@@ -117,6 +117,133 @@ function setCharAt(str,index,chr) {
 
 //Makes the text box unusable after the game has been won/lost.
 function textStop() {
-    if (guessCount == 6, gameWord == targetWord);
+    if (guessCount == 11, gameWord == targetWord);
         document.getElementById("userGuess").disabled = true;
+}
+
+
+//Canvas Hangman
+//
+
+var functionsArray = [
+    hangmanStandBottom,
+    hangmanStandSide,
+    hangmanStandTop,
+    hangmanStandCorner,
+    hangmanNoose,
+    hangmanHead,
+    hangmanBody,
+    hangmanLeftLeg,
+    hangmanRightLeg,
+    hangmanLeftArm,
+    hangmanRightArm
+]
+
+var c = document.getElementById("hangmanCanvas");
+var canvasWidth = c.width
+var canvasHeight = c.height
+var headCenterY = 100
+var headRadius = 25
+var legEndY = 400
+var armStartX = 250
+var armStartY = 200
+var armHeight = 200
+
+//Stand Bottom
+function hangmanStandBottom(){
+    var ctx = c.getContext("2d");
+    ctx.beginPath();
+    ctx.moveTo(500, 500);
+    ctx.lineTo(175, 500);
+    ctx.stroke();
+}
+
+//Stand Side
+function hangmanStandSide(){
+    var ctx = c.getContext("2d");
+    ctx.beginPath();
+    ctx.moveTo(500, 500);
+    ctx.lineTo(500, 0);
+    ctx.stroke();
+}
+
+//Stand Top
+function hangmanStandTop(){
+    var ctx = c.getContext("2d");
+    ctx.beginPath();
+    ctx.moveTo(250, 0);
+    ctx.lineTo(500, 0);
+    ctx.stroke();
+}
+
+//Stand Corner
+function hangmanStandCorner(){
+    var ctx = c.getContext("2d");
+    ctx.beginPath();
+    ctx.moveTo(450, 0);
+    ctx.lineTo(500, 50);
+    ctx.stroke();
+}
+
+//Noose
+function hangmanNoose(){
+    var ctx = c.getContext("2d");
+    ctx.beginPath();
+    ctx.moveTo(250, 0);
+    ctx.lineTo(250, headCenterY - headRadius);
+    ctx.stroke();
+}
+
+//Head
+function hangmanHead(){
+    var c = document.getElementById("hangmanCanvas");
+    var ctx = c.getContext("2d");
+    ctx.beginPath();
+    ctx.arc(canvasWidth / 2,headCenterY,headRadius,0,2*Math.PI);
+    ctx.stroke();
+}
+
+//Body
+function hangmanBody(){
+    var ctx = c.getContext("2d");
+    ctx.beginPath();
+    ctx.moveTo(canvasWidth / 2, headCenterY + headRadius);
+    ctx.lineTo(canvasWidth / 2, canvasHeight / 2);
+    ctx.stroke();
+}
+
+//Left Leg
+function hangmanLeftLeg(){
+    var ctx = c.getContext("2d");
+    ctx.beginPath();
+    ctx.moveTo(canvasWidth / 2, canvasHeight / 2);
+    ctx.lineTo(150, legEndY);
+    ctx.stroke();
+}
+
+//Right Leg
+function hangmanRightLeg(){
+    var ctx = c.getContext("2d");
+    ctx.beginPath();
+    ctx.moveTo(canvasWidth / 2, canvasHeight / 2);
+    ctx.lineTo(350, legEndY);
+    ctx.stroke();
+}
+
+//Left Arm
+function hangmanLeftArm(){
+    var ctx = c.getContext("2d");
+    ctx.beginPath();
+    ctx.moveTo(armStartX, armStartY);
+    ctx.lineTo(150, armHeight);
+    ctx.stroke();
+}
+
+//Right Arm
+function hangmanRightArm(){
+    var ctx = c.getContext("2d");
+    ctx.beginPath();
+    ctx.moveTo(armStartX, armStartY);
+    ctx.lineTo(350, armHeight);
+    ctx.stroke();
 }
