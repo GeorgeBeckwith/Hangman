3cfc36c1abd8e404b86333e1183916c8df9020e4
George Beckwith <george.jbeckwith@googlemail.com>
2017-07-20 10:30:06 +0100

    Just use express app to serve assets

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 var express = require('express')
-var serveStatic = require('serve-static')
  
 var app = express()
  
-app.use(serveStatic('.', {'index': ['index.html']}))
+app.use('/', express.static(__dirname + '/'));
+
+app.get('/*', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+
 app.listen(process.env.PORT || 3000)
