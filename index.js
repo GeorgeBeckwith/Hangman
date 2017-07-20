555fcd4ea1004cec2e93cca42fce9e58abaab162
George Beckwith <george.jbeckwith@googlemail.com>
2017-07-20 10:38:33 +0100

    Attempt to fix issues on heroku deploy

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var express = require('express')
+var path = require('path')
  
 var app = express()
  
-app.use('/', express.static(__dirname + '/'));
+app.use('/', express.static(__dirname));
 
-app.get('/*', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+app.get('*', function(req, res){
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
-
 
 app.listen(process.env.PORT || 3000)
